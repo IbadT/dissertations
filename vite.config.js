34fffbc9e669e88d96b23ca57fcf252d8b526bb9
Eduard Kononovich <101503726+IbadT@users.noreply.github.com>
2024-09-01 00:33:32 +0300

    fix

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://github.io/IbadT/dissertations.git",
+  // base: "https://github.io/IbadT/dissertations.git",
+  base: "https://IbadT.github.io/dissertations",
   plugins: [react()],
 })

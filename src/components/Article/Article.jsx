a32cad18789d6666b0c21180405a5042ef5c6684
Eduard Kononovich <101503726+IbadT@users.noreply.github.com>
2024-11-26 10:59:40 +0300

    dissertation

--- src/components/Article/Article.jsx
+++ src/components/Article/Article.jsx
@@ -1,9 +1,16 @@
-import { Flex } from "antd"
+import { Flex } from "antd";
+import WordViewer from "./WordViewer";
 
 export const Article = () => {
     return (
-        <Flex justify="center">
-            Article
+        <Flex vertical justify="center" >
+            <Flex justify="center" style={{ margin: "2vmin" }}>
+                <a style={{ border: "2px solid black", padding: "1vmin", borderRadius: "20px" }} download="/Article.docx">Скачать статью</a>
+                {/* <Link href="/Диссертация.docx" download color="blue.500">Открыть выпускную работу</Link> */}
+            </Flex>
+            <Flex justify="center" style={{ padding: "0 20px" }}>
+                <WordViewer />
+            </Flex>
         </Flex>
     )
 };

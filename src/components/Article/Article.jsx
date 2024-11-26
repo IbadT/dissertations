cf9a0ddeed394de0cdce659bb574988940b86ff9
Eduard Kononovich <101503726+IbadT@users.noreply.github.com>
2024-11-26 11:56:51 +0300

    article

--- src/components/Article/Article.jsx
+++ src/components/Article/Article.jsx
@@ -12,12 +12,12 @@ export const Article = () => {
         //         <WordViewer />
         //     </Flex>
         // </Flex>
-        <Flex justify="center" align="center" minHeight="100vh" flexDirection="column" bg="gray.100" p={4} >
+        <Flex vertical justify="center" align="center" minHeight="100vh" flexDirection="column" bg="gray.100" p={4} >
             <Flex justify="center" style={{ margin: "2vmin" }}>
                 <a style={{ border: "2px solid black", padding: "1vmin", borderRadius: "20px" }} download="/Article.docx">Скачать статью</a>
                 {/* <Link href="/Диссертация.docx" download color="blue.500">Открыть выпускную работу</Link> */}
             </Flex>
-            <Flex justify="center" style={{ padding: "0 20px" }}>
+            <Flex justify="center" style={{ padding: "0 4vmin" }}>
                 <WordViewer />
             </Flex>
         </Flex>
--- src/components/Dissertation/Dissertation.jsx
+++ src/components/Dissertation/Dissertation.jsx
@@ -13,14 +13,14 @@ export const Dissertation = () => {
         //     </Flex>
         // </Flex>
 
-        <Flex justify="center" align="center" minHeight="100vh" flexDirection="column" bg="gray.100" p={4} > 
+        <Flex vertical justify="center" align="center" minHeight="100vh" flexDirection="column" bg="gray.100" p={4} > 
             <Flex justify="center" style={{ margin: "2vmin" }}> 
                 <a style={{ border: "2px solid black", padding: "1vmin", borderRadius: "20px", textAlign: "center" }} 
                     href="/Диссертация.docx" download > Скачать выпускную работу 
                 </a> 
                 {/* <Link href="/Диссертация.docx" download color="blue.500">Открыть выпускную работу</Link> */} 
             </Flex> 
-            <Flex justify="center" style={{ padding: "0 20px" }}> 
+            <Flex justify="center" style={{ padding: "0 4vmin" }}> 
                 <WordViewer /> 
             </Flex> 
         </Flex>
